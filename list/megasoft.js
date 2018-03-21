79269c3d3da952fbcc4a171e2225c084d4501cf1
Mohinabonu <apple@MacBook-Air.local>
2018-03-21 19:23:46 +0500

    first commit

---
+++ list/megasoft.js
@@ -0,0 +1,12 @@
+const START_URL = "http://megasoft.uz/",
+    SELECTOR = {  
+    title: '.title',
+    description:'body table:nth-child(6) td:nth-child(2) | removeDown | whiteSpace',
+    downloadLink: ['a[href^="http://megasoft.uz/get"]@href'],
+    publishDate: 'body table[width="300"] tr:nth-child(5) td:nth-child(2) | date',
+    pageLinks: ['a[href^="' + START_URL + '"]:not([href^="http://megasoft.uz/get"]):not([href*="?sort"]):not([href*="#"])@href']
+  },
+  TARGET = 'downloadLink',
+  ENCODING = 'windows-1251';
+
+require('../')(START_URL,TARGET,SELECTOR,ENCODING);
